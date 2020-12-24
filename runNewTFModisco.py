f281c3492a0048d6438ed28e48a49a51d0021cb9
imk1 <ikaplow@stanford.edu>
2020-12-24 14:11:29 -0800

    I added a wrapper for running TF-MoDISco.

---
+++ runNewTFModisco.py
@@ -0,0 +1,138 @@
+import sys
+import argparse
+import h5py
+import numpy as np
+import pybedtools as bt
+import modisco
+import modisco.backend
+import modisco.nearest_neighbors
+import modisco.affinitymat
+import modisco.tfmodisco_workflow.seqlets_to_patterns
+import modisco.tfmodisco_workflow.workflow
+import modisco.aggregator
+import modisco.cluster
+import modisco.core
+import modisco.coordproducers
+import modisco.metaclusterers
+import modisco.util
+from collections import OrderedDict
+
+def parseArgument():
+        # Parse the input
+        parser=argparse.ArgumentParser(description=\
+                        "Run the new TF-Modisco on deepLIFT scores")
+        parser.add_argument("--sequenceLength", type=int, required=False, default=1000, \
+                        help='Length of the sequence that was given to the model')
+        parser.add_argument("--maxPeakLength", type=int, required=False, default=None, \
+                        help='Maximum length of peaks that will be inputted into the model')
+        parser.add_argument("--peakInfoFileName", required=True,\
+                        help='narrowPeak file with the optimal peaks or name of file with list of summits')
+        parser.add_argument("--DESeq2OutputFileName", required=False,\
+                        help='name of the file with the output from DESeq2, use only if doing differential peaks')
+        parser.add_argument("--genomeFileName", required=True,\
+                        help='name of file with the genome sequence')
+	parser.add_argument("--chroms", required=False, action='append', \
+                        default = ["chr8", "chr9"], \
+                        help='chromosomes for which deepLIFT scores will be generated')
+	parser.add_argument("--differentialPeaks", action='store_true', required=False,\
+                        help='Data is from a differential peak tasks, so load files for DESeq2')
+        parser.add_argument("--negSet", action='store_true', required=False,\
+                        help='Data is from a differential peak tasks and deepLIFT should be run on the negative set')
+	parser.add_argument("--deepLiftScoresFileName", required=True,\
+                        help='hdf5 file with deepLIFT scores')
+	parser.add_argument("--TFModiscoResultsFileName", required=True,\
+                        help='hdf5 file with TF Modisco results')
+	parser.add_argument("--codePath", required=False, default="/srv/scratch/imk1/TFBindingPredictionProject/src/", \
+		help='Path to code that will be used')
+        options = parser.parse_args()
+        return options
+
+def runNewTFModisco(options):
+	# Run the new TF-Modisco on deepLIFT scores
+	# One-hot-encode the true positives
+	peakInfoFileNameElements = options.peakInfoFileName.split(".")
+        peakInfoFileNamePrefix = ".".join(peakInfoFileNameElements[0:-2])
+        # Load the input data
+        trueLabelIndices = np.empty((1,1))
+        trueLabelRegions = bt.BedTool()
+	sys.path.insert(0,options.codePath + "/deeplearning")
+	print (options.chroms)
+        from sequenceOperations import makeSummitPlusMinus
+	X = None
+	Y = None
+        if (not options.differentialPeaks):
+                # The peaks are not differential peaks
+                assert (not options.negSet)
+                optimalBedFileName = peakInfoFileNamePrefix + ".bed"
+                summitPlusMinus =\
+                        makeSummitPlusMinus(optimalBedFileName, createOptimalBed=False, dataShape=(1,4,options.sequenceLength), bedFilegzip=False, \
+				chroms=options.chroms, maxPeakLength=options.maxPeakLength)
+                from sequenceOperations import makePositiveSequenceInputArraysFromNarrowPeaks
+                X, Y =\
+                        makePositiveSequenceInputArraysFromNarrowPeaks(options.peakInfoFileName, options.genomeFileName, createOptimalBed=False, \
+                                dataShape=(1,4,options.sequenceLength), chroms=options.chroms, multiMode=False, maxPeakLength=options.maxPeakLength)
+        else:
+                # The peaks are differential peaks, so only the summits have been included
+                summitPlusMinus =\
+			makeSummitPlusMinus(options.peakInfoFileName, createOptimalBed=False, dataShape=(1,4,options.sequenceLength), summitCol=1, \
+				startCol=None)
+                from sequenceOperations import makeSequenceInputArraysFromDifferentialPeaks
+                X, Y, _, _, _, indices =\
+                        makeSequenceInputArraysFromDifferentialPeaks(options.DESeq2OutputFileName, options.genomeFileName, options.peakInfoFileName, \
+                                (1,4,options.sequenceLength), createOptimalBed=False, backgroundSummitPresent=False, backgroundSummitOnly=True, \
+				createModelDir=False, chroms=options.chroms, bigWigFileNames=[], multiMode=False, streamData=False, dataFileName="", \
+				RC=False)
+        data = None
+	if options.negSet:
+		# Run TF-Modisco on the negatives
+		data = X[Y == 0,:,:,:]
+	else:
+		# Run TF-Modisco on the positives
+		data = X[Y == 1,:,:,:]
+	dataReshape = np.zeros((data.shape[0], data.shape[3], data.shape[2]))
+	for i in range(data.shape[0]):
+		# Iterate through the sequences and re-format them to be in the format needed for the new TF-Modisco
+		for j in range(data.shape[3]):
+			# Iterate through the sequences and re-format the channels
+			for k in range(data.shape[2]):
+				# Iterate through the channels and enter the re-formatted entries
+				dataReshape[i,j,k] = data[i,0,k,j]
+	# Prepare the deepLIFT scores for TF-Modisco
+	task_to_scores = OrderedDict()
+	task_to_hyp_scores = OrderedDict()
+	f = h5py.File(options.deepLiftScoresFileName, "r")
+	tasks = f["contrib_scores"].keys()
+	for task in tasks:
+		# Iterate through the tasks and put the importance and hypothetical importance scores for each task into the dictionaries
+		task_to_scores[task] = [np.array(x) for x in f['contrib_scores'][task][:]]
+		task_to_hyp_scores[task] = [np.array(x) for x in f['hyp_contrib_scores'][task][:]]
+	# Run TF-Modisco
+	reload(modisco)
+	reload(modisco.backend.tensorflow_backend)
+	reload(modisco.backend)
+	reload(modisco.nearest_neighbors)
+	reload(modisco.affinitymat.core)
+	reload(modisco.affinitymat.transformers)
+	reload(modisco.tfmodisco_workflow.seqlets_to_patterns)
+	reload(modisco.tfmodisco_workflow.workflow)
+	reload(modisco.aggregator)
+	reload(modisco.cluster.core)
+	reload(modisco.cluster.phenograph.core)
+	reload(modisco.cluster.phenograph.cluster)
+	reload(modisco.core)
+	reload(modisco.coordproducers)
+	reload(modisco.metaclusterers)
+	tfmodisco_results =\
+		modisco.tfmodisco_workflow.workflow.TfModiscoWorkflow(sliding_window_size=21, flank_size=10, target_seqlet_fdr=0.2, \
+			seqlets_to_patterns_factory =\
+				modisco.tfmodisco_workflow.seqlets_to_patterns.TfModiscoSeqletsToPatternsFactory(trim_to_window_size=20, \
+					initial_flank_to_add=5, kmer_len=8, num_gaps=1, num_mismatches=0, final_min_cluster_size=20))\
+		(task_names = ["task0"], contrib_scores = task_to_scores, hypothetical_contribs = task_to_hyp_scores, one_hot = dataReshape)
+	# Save the results
+	reload(modisco.util)
+	grp = h5py.File(options.TFModiscoResultsFileName)
+	tfmodisco_results.save_hdf5(grp)
+
+if __name__ == "__main__":
+        options = parseArgument()
+        runNewTFModisco(options)
